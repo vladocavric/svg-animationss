1105232d4e7201820727241dd24ba3d8c27fe4af
Vlado Cavric <vladocavric@gmail.com>
2023-12-17 20:00:32 +0100

    logo animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import './App.scss'
+
+import Logo from './assets/Logo'
 
 function App() {
 
 
   return (
     <>
-    
+      <Router>
+      <div>
+        {/* Navigation links */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/logo">Logo</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Route configurations */}
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/logo" element={<Logo />} />
+          <Route path="/contact" element={<Logo />} />
+        </Routes>
+      </div>
+    </Router>
     </>
   )
 }
